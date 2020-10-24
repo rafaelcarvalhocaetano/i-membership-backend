ed90c518848019dbc973b67a11cbcb6c5025f902
rafael <rapha.pse@outlook.com>
2020-10-23 21:59:42 -0300

    Fix readm and routers

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # i-membershp-backend
-i-membershp-backend
+
+### - Rotas
+ - /user ``` get with filters and pagination ```
+ - /user/all ``` get  ```
+ - /user/:id ``` get  ```
+ - /user     ``` post  ```
+ - /user/:id ``` put  ```
+ - /user/:id ``` delete  ```
--- routers/UserRouter.ts
+++ routers/UserRouter.ts
@@ -9,7 +9,7 @@ export class UserRouter {
     this.router.route('/user').get(UserController.findAllUsers);
     this.router.route('/user/all').get(UserController.findAll);
     this.router.route('/user/:id').get(UserController.findByUserId);
-    this.router.route('/user/').post(UserController.createUser);
+    this.router.route('/user').post(UserController.createUser);
     this.router.route('/user/:id').put(UserController.updateUser);
     this.router.route('/user/:id').delete(UserController.deleteUser);
   }
